93edde70d384d264852a58a9ba025ce3bea05c54
Miriam <miriam_ashton@brown.edu>
2014-05-02 18:11:10 -0400

    fixed timestamp display

--- js/inbox_script.js
+++ js/inbox_script.js
@@ -49,11 +49,73 @@ function replaceInboxMessages(newMessages) {
         $(this).find('.subject').text(newMessages[index].subject);
 
         // replace timestamp
+
         var timestamp = newMessages[index].timestamp
-        $(this).find('.timestamp').text(timestamp.substring(5,10));
+        getFormattedDate(timestamp);
+        $(this).find('.timestamp').text(getFormattedDate(timestamp));
 
         // replace message
         $(this).find('.message').text(newMessages[index].message);
     });
+}
+
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+    'Oct', 'Nov', 'Dec'];
+function getFormattedDate(timestamp) {
+    var month = parseInt(timestamp.substring(5,7));
+    var day = parseInt(timestamp.substring(8,10));
+    var year = parseInt(timestamp.substring(0, 4));
+    var hr = parseInt(timestamp.substring(11,13));
+    var min = parseInt(timestamp.substring(14,16));
+    var displayString = months[month-1]+" "+day;
+
+
+
+
+    // construct email's date object (time received)
+    var email = new Date();
+    email.setDate(day);
+    email.setMonth(month-1);
+    email.setFullYear(year);
+    email.setHours(hr);
+    email.setMinutes(min);
+    // dates are given in GMT so subtract offset
+    var offset = new Date().getTimezoneOffset();
+    offset = offset/60;
+    email.addHours(-offset); 
+
+    console.log("EMAIL RECEIVED AT "+email);
+    // if same date, displace time isntead
+    var today = new Date();
+    if(today.getDate() === email.getDate() && 
+        today.getMonth() === email.getMonth() &&
+        today.getFullYear() === email.getFullYear()) {
+        
+        hr = email.getHours();
+        min = email.getMinutes();
+        var ext = 'am';
+        
+        if(hr > 12) {
+            hr = hr % 12;
+            ext = 'pm';
+        }
+        else if (hr === 0){
+            hr = 12;
+            ext = 'am';
+        }
+        if(min<10) {
+            min = "0"+min;
+        }
+
+        displayString = hr+":"+min+ext;
+    }
+
+    return displayString;
+}
+
+
+Date.prototype.addHours = function(h) {    
+   this.setTime(this.getTime() + (h*60*60*1000)); 
+   return this;   
 }
 
