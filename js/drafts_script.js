7bd707c340e656eb2d7235a7eeb739eb5a021efb
Miriam <miriam_ashton@brown.edu>
2014-05-15 18:11:20 -0400

    clear

--- js/drafts_script.js
+++
@@ -1,8 +0,0 @@
-
-
-function openDraft(draft) {
-	// TODO should be going to compose, not read. drafts must have uids for this.
-   	window.location.href = '/compose/%5BGmail%5D%2FDrafts/'+$(draft).attr('uid');
-
-}
-
